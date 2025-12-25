d8fc329fb4ca988a20f6c96f18188d2a81507826
SangwanYu <ryusangwan12@gmail.com>
2025-12-25 18:14:09 +0900

    ✨ Feat: 스크롤 유지 기능 추가

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 
 const RootLayout = () => {
   return (
     <div className="flex min-h-screen flex-col items-center">
+      <ScrollRestoration />
       <Navbar />
       <main className="flex w-full flex-1 justify-center">
         <Outlet />
