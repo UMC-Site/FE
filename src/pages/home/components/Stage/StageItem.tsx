37abd26c4b3017638f0f29394b1f485b2a44660a
hyeseong <tell280210@gmail.com>
2026-01-05 17:25:58 +0900

    🗃️ Comment: 기수별 인원 임시 주석처리

--- src/pages/home/components/Stage/StageItem.tsx
+++ src/pages/home/components/Stage/StageItem.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import PeopleIcon from "@/assets/images/icons/people.svg?react";
+// import PeopleIcon from "@/assets/images/icons/people.svg?react";
 import type { StageData } from "../../data";
 import { getFadeInUpClass } from "../../utils";
 
@@ -23,6 +23,7 @@ const StageItem = ({ stage, isVisible, delay }: StageItemProps) => (
     <p className="mt-[0.6rem] whitespace-nowrap font-semibold text-[2.4rem] text-white leading-[2.4rem] max-sm:mt-[0.588rem] max-sm:text-[1.4rem] max-sm:leading-[1.7rem]">
       {stage.title}
     </p>
+    {/* 인원수 표시
     <div className="mt-[2.4rem] flex h-[4.6rem] w-44 items-center justify-center rounded-lg border border-primary-900 transition-all duration-300 max-sm:mt-[1.4rem] max-sm:h-[2.8rem] max-sm:w-24 max-sm:rounded-sm">
       <div className="flex w-[6.4rem] items-center justify-between max-sm:w-[3.8rem]">
         <PeopleIcon className="h-8 w-8 max-sm:h-[1.2rem] max-sm:w-[1.2rem]" />
@@ -31,6 +32,7 @@ const StageItem = ({ stage, isVisible, delay }: StageItemProps) => (
         </p>
       </div>
     </div>
+    */}
   </div>
 );
 
