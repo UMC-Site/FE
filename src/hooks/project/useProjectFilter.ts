4ebf044f7b54a4c567455289cb2674bfe6fc0378
SangwanYu <ryusangwan12@gmail.com>
2026-01-06 15:42:57 +0900

    refactor: 기수 선택 인덱스 사용

--- src/hooks/project/useProjectFilter.ts
+++ src/hooks/project/useProjectFilter.ts
@@ -1,9 +1,9 @@
 import { useMemo, useState } from "react";
 import type { FilterType, Project } from "@/types/project";
-import { getFilteredProjects } from "@/utils/Project";
+import { getFilteredProjects, GENERATION_TYPES } from "@/utils/Project";
 
 export const useProjectFilter = () => {
-  const [selectedFilter, setSelectedFilter] = useState<FilterType>("9기");
+  const [selectedFilter, setSelectedFilter] = useState<FilterType>(GENERATION_TYPES[0]);
 
   const filteredProjects: Project[] = useMemo(() => {
     return getFilteredProjects(selectedFilter);
