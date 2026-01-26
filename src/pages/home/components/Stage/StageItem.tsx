a87b3befd96fd1968f1e0b62eb685c7de2b98026
hyeseong <tell280210@gmail.com>
2026-01-25 22:05:32 -0600

    🎨 Design: home stageBar 크기 조정

--- src/pages/home/components/Stage/StageItem.tsx
+++ src/pages/home/components/Stage/StageItem.tsx
@@ -12,15 +12,15 @@ interface StageItemProps {
 const StageItem = ({ stage, isVisible, delay }: StageItemProps) => (
   <div
     className={clsx(
-      "flex w-[14.4rem] flex-col items-center text-center max-sm:w-28 max-lg:w-40",
+      "flex w-full flex-col items-center text-center",
       getFadeInUpClass(isVisible)
     )}
     style={{ animationDelay: delay }}
   >
-    <p className="font-normal text-[2rem] text-white leading-[2.4rem] max-sm:text-[1rem] max-sm:leading-[1.2rem] max-lg:text-[1.6rem]">
+    <p className="font-normal text-[2rem] text-white max-sm:text-[0.9rem] sm:text-[1.1rem] md:text-[1.4rem] lg:text-[1.8rem]">
       {stage.date}
     </p>
-    <p className="mt-[0.6rem] whitespace-nowrap font-semibold text-[2.4rem] text-white leading-[2.4rem] max-sm:mt-[0.588rem] max-sm:text-[1.4rem] max-sm:leading-[1.7rem] max-lg:text-[1.8rem]">
+    <p className="mt-[0.6rem] whitespace-nowrap font-semibold text-[2.4rem] text-white max-sm:mt-[0.588rem] max-sm:text-[1.1rem] sm:text-[1.3rem] md:text-[1.6rem] lg:text-[2rem]">
       {stage.title}
     </p>
     {/* 인원수 표시
