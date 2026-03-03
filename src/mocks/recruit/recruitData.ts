10e4b1408160ae1f75af5137aa4e6bcd3e623b37
SangwanYu <ryusangwan12@gmail.com>
2026-03-03 10:00:04 +0900

    ♻️ Refactor: 10기 추가모집 정보 반영

--- src/mocks/recruit/recruitData.ts
+++ src/mocks/recruit/recruitData.ts
@@ -4,13 +4,13 @@ const RecruitData = {
   formUrl: "https://forms.gle/os6M2ScMrkh389DA8",
   activeDate: { id: 1, title: "2026.03 ~ 2026.08 [약 6개월]" },
   part: [
-    { id: 1, title: "PM" },
-    { id: 2, title: "Design" },
+    // { id: 1, title: "PM" },
+    // { id: 2, title: "Design" },
     { id: 3, title: "Android" },
-    { id: 4, title: "iOS" },
+    // { id: 4, title: "iOS" },
     { id: 5, title: "Web" },
-    { id: 6, title: "Springboot" },
-    { id: 7, title: "Node.js" },
+    // { id: 6, title: "Springboot" },
+    // { id: 7, title: "Node.js" },
   ],
 
   people: [
@@ -25,18 +25,18 @@ const RecruitData = {
   recruitDate: [
     {
       id: 1,
-      step: "서류 모집",
-      date: "2026.02.16(월) - 2026.02.27(금)",
+      step: "추가모집 서류 지원",
+      date: "2026.03.02(월) ~ 2026.03.05(목)",
     },
     {
       id: 2,
-      step: "서류 합격 발표",
-      date: "2026.03.01(일)",
+      step: "서류 합격 발표 및 면접 일정 조율",
+      date: "2026.03.02(월) ~ 2026.03.05(목)",
     },
     {
       id: 3,
       step: "면접 일자",
-      date: "2026.03.02(월) ~ 2026.03.06(금)",
+      date: "2026.03.03(화) ~ 2026.03.06(금)",
     },
     {
       id: 4,
